d946d87ff733a22bc16aee79ae7e418b87ee97a0
chong <windcarrier@126.com>
2018-08-13 18:02:25 +0800

    ENTER

---
+++ DotNetCore/Web/ASP/src/WebAppTest1/WebAppTest1/AdRotator.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdRotator.aspx.cs" Inherits="WebAppTest1.AdRotator" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h3>File Upload Test</h3>
+    <br />
+    <br />
+    <asp:FileUpload ID = "fileUpload1" runat="server" />
+    <br />
+    <br />
+    <asp:Button ID = "btnSave" runat = "server"  OnClick="btnSave_Click" Text= "Save" style = "width:85px" />
+    <br />
+    <asp:Label ID = "Message" runat = "server"  />
+    </div>
+    </form>
+</body>
+</html>
---
+++ DotNetCore/Web/ASP/src/WebAppTest1/WebAppTest1/AdRotator.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace WebAppTest1
+{
+    public partial class AdRotator : System.Web.UI.Page
+    {
+        StringBuilder sb = new StringBuilder();        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (fileUpload1.HasFile)
+            {
+                try
+                {
+                    sb.AppendFormat("Uploading File: {0}", fileUpload1.FileName);
+                    fileUpload1.SaveAs("C:\\SaveFiles\\" + fileUpload1.FileName);
+                    sb.AppendFormat("<br /> Save As {0}", fileUpload1.PostedFile.FileName);
+                    sb.AppendFormat("<br /> File type: {0}", fileUpload1.PostedFile.FileName);
+                    sb.AppendFormat("<br /> File name: {0}", fileUpload1.PostedFile.FileName);
+                    sb.AppendFormat("<br /> File length: {0} bit", fileUpload1.PostedFile.ContentLength);
+
+                    Message.Text = sb.ToString();
+                }
+                catch (Exception ex)
+                {
+                    sb.Append("<br /> Error <br />");
+                    sb.AppendFormat("Unable to save file <br /> {0}", ex.Message);
+                }     
+            }
+            else
+            {
+                Message.Text = sb.ToString();
+            }
+        } 
+    }
+}
