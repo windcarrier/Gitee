bc3b4278e04521cb4174398df2bf74177c2b4f55
chong <windcarrier@126.com>
2018-08-22 10:53:58 +0800

    enter

---
+++ DotNetCore/Web/ASP/src/WebAppTest1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            
+            for (int i = 0; i < nums.Count()-1; i++)
+            {
+                for (int j = i + 1; j < nums.Count(); j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        int[] IndexList = {nums[i],nums[j]};
+                        return IndexList;
+                    }
+                }
+            }
+                return null;
+        }
+    }
+}
