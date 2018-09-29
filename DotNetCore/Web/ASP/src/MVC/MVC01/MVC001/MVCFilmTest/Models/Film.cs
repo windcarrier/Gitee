aea9a0c38a280b1cbeb6cb1f16388fcbdbbbe9c9
chong <windcarrier@126.com>
2018-09-29 17:59:30 +0800

    enter

--- DotNetCore/Web/ASP/src/MVC/MVC01/MVC001/MVCFilmTest/Models/Film.cs
+++ DotNetCore/Web/ASP/src/MVC/MVC01/MVC001/MVCFilmTest/Models/Film.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,9 @@ namespace MVCFilmTest.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
         public DateTime DateTime { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
